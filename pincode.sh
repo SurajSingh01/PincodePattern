de2a27521366f837fa87ac3bcaccdb23678678f6
SurajSingh01 <singhsurajd03@gmail.com>
2020-06-07 22:48:58 +0530

    Validate only 6 digit number

--- pincode.sh
+++ pincode.sh
@@ -1,2 +1,10 @@
 #!/bin/bash -x
 echo "Welcome in process of finding Pincode"
+read -p "Enter pincode :- " pincode
+pat="[0-9]{6}"
+if [[ $pincode =~ $pat ]]
+then
+		echo "Valid"
+else
+		echo "Invalid"
+fi
